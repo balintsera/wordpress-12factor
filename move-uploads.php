b9881cd144fa388d3f65f7f38cd0779dbce35149
Séra Bálint <sera.balint@e-vista.hu>
2016-09-30 19:38:30 +0200

    move uploads to and from wordpress

---
+++ move-uploads.php
@@ -0,0 +1,32 @@
+#!/bin/php
+<?php
+
+ini_set('register_argc_argv', 0);
+if (!isset($argc) || is_null($argc)) die('CLI only');
+
+$upload_dir = dirname(dirname(__FILE__)). DIRECTORY_SEPARATOR . 'uploads';
+
+
+// Apache could handle symlinked directories 
+// but php -S can't and we want uniform developer and deploy experiences
+// throughout all OS-es
+/* 
+$os = strtolower(php_uname('s')); 
+
+if($os === 'darwin' || strpos($os, 'win') === false) {
+  echo 'not win, won\'t move uploads dir';
+  return; 
+} 
+*/
+// Windows
+switch($argv[1]){
+    case 'pre':
+      rename("wordpress/wp-content/uploads", "uploads");
+    break;
+
+    case 'post':
+      rename("uploads", "wordpress/wp-content/uploads");
+    break;
+    default:
+      throw new Exception('No argument or wrong argument:' . $argv[1]);
+}
