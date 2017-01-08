5fc1529a84f56fe9e2dff8cb7a2240e77417e675
Séra Bálint <sera.balint@e-vista.hu>
2017-01-08 08:41:53 +0100

    php 7 update

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -4,8 +4,3 @@
 [[ ! -e /.dockerenv ]] && [[ ! -e /.dockerinit ]] && exit 0
 
 set -xe
-
-# Install mysql driver
-# Here you can install any other extension that you need
-docker-php-ext-install pdo_mysql
-#docker-php-ext-install gd
